6190cffd11193043e57cf724e601fcc5c08cdf9d
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2018-05-01 10:22:49 +0300

    Build task

--- tools/eureka-server/src/main/java/ofedorova/EurekaServerApplication.java
+++ tools/eureka-server/src/main/java/ofedorova/EurekaServerApplication.java
@@ -8,9 +8,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 @EnableEurekaClient
-public class DemoDiscoveryServerApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoDiscoveryServerApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
